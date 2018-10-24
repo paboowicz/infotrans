7ef06c49bbbc5055645bd77cdff002ef2a67bcf7
Paboowicz <paboowicz@gmail.com>
2018-10-24 10:48:50 +0200

    google maps api w index + gps w main.js

--- js/main.js
+++ js/main.js
@@ -608,7 +608,7 @@
             var mapOptions = {
                 zoom: 15,
                 scrollwheel: false,
-                center: new google.maps.LatLng(-37.81618, 144.95692),
+                center: new google.maps.LatLng(50.019449, 21.9654965),
                 styles: [{
                     stylers: [{
                         saturation: -100
